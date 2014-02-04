ae1952292a927f5952e11708af2d9a2a92296970
chong <lichong_79@hotmail.com>
2014-02-04 15:04:29 -0500

    add field error in template;change team member page content

--- zenith_new/form.py
+++ zenith_new/form.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 import re
 
 def validate_phonenumber(value):
-    p = re.compile('(?:(?:(\s*\(?([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2‌​-9][02-8]1|[2-9][02-8][02-9]))\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9]‌​[02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})')
+    p = re.compile('^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$')
     if not p.match(value):
         raise ValidationError(u'%s is not a valid phone number' % value)
 
--- zenith_new/views.py
+++ zenith_new/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from zenith_new.form import ContactForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def home(request):
     if request.method== 'POST':
        form = ContactForm(request.POST)
+
        if form.is_valid():
-          pass
+          return HttpResponseRedirect(reverse('home'))
     else:
        form=ContactForm(auto_id=False)
-    return render(request,'index.html',{'form':form,})
+    return render(request,'index.html',{'form':form})
