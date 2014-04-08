8b47b56ad2f2a4470c8f027012538dbd1b5b87d7
user <user@lubuntu.(none)>
2014-04-07 20:02:49 -0400

    form error display in ajax.py

--- ajax/ajax.py
+++ ajax/ajax.py
@@ -3,6 +3,7 @@ from dajax.core import Dajax
 from dajaxice.utils import deserialize_form
 from zenith_new.form import ContactForm
 from django.core.mail import send_mail
+from django.utils.translation import ugettext as _
 
 @dajaxice_register
 def send_form(request, form):
@@ -10,12 +11,12 @@ def send_form(request, form):
     form = ContactForm(form)
 
     if form.is_valid():
-        #cd = form.cleaned_data
-        #send_mail(cd['phone'],cd['message'],cd['name']+'<'+cd['email']+'>',[r'zenith.technology.llc@gmail.com'], fail_silently=False)        
-        dajax.alert("Form is valid")
+        cd = form.cleaned_data
+        send_mail(cd['phone'],cd['message'],cd['name']+'<'+cd['email']+'>',[r'zenith.technology.llc@gmail.com'], fail_silently=False)        
+        dajax.alert(_("Thank you, we will contact you soon!"))
     else:
-         dajax.alert("Form is not valid")
-
+        error_list=form.errors.items() 
+        dajax.alert(str(error_list))
     return dajax.json()
 
 
